bf86ebe7ab73ae42cca0a3b4560f6edbba2f0b65
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-26 19:16:40 +0200

    added assets

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./Navbar";
 import PropTypes from "prop-types";
+import logo from "../assets/logo.svg";
 function Header({ isAuthenticated, handleLogout }) {
   return (
     <header>
       <div>
-        <img />
-        <h1 className="title">Budgetix</h1>
+        <div className="header">
+          <img
+            src={logo}
+            alt="logo representant un billet"
+            className="logo-budgetix"
+          />
+          <h1 className="title">Budget app</h1>
+        </div>
         <Navbar isAuthenticated={isAuthenticated} handleLogout={handleLogout} />
       </div>
     </header>
