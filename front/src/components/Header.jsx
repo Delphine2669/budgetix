66a1e1b2d21dce56baf2da8ce199f33c3cad828d
@Delphine2669 <delphine.thevenet2669@gmail.com>
2025-03-04 20:13:06 +0100

    modified theme preferences

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,15 +1,26 @@
 // import Navbar from "./Navbar";
 // import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 import "./Header.css";
 import lightLogo from "../assets/logoLight.svg";
+import darkLogo from "../assets/logoD.png";
+
 // function Header({ isAuthenticated, handleLogout }) {
 function Header() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  useEffect(() => {
+    const prefersDarkTheme = window.matchMedia(
+      "(prefers-color-scheme:dark)"
+    ).matches;
+
+    setIsDarkTheme(prefersDarkTheme);
+  }, []);
   return (
     <header>
       <div>
         <div className="header">
           <img
-            src={lightLogo}
+            src={isDarkTheme ? darkLogo : lightLogo}
             alt="logo representant un billet"
             className="logo-budgetix"
           />
